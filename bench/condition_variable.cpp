ac803bf1b1bcf7d4d4f0c4f27a48aa0815a09aa8
Klemens <klemens.d.morgenstern@gmail.com>
2023-03-08 12:09:36 +0800

    Added simple bench.

---
+++ bench/condition_variable.cpp
@@ -0,0 +1,141 @@
+// Copyright (c) 2023 Klemens D. Morgenstern
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/asem/condition_variable.hpp>
+
+#if defined(BOOST_ASEM_STANDALONE)
+#include <asio/steady_timer.hpp>
+#include <asio/compose.hpp>
+#include <asio/detached.hpp>
+
+#else
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/compose.hpp>
+#include <boost/asio/detached.hpp>
+#endif
+
+using namespace BOOST_ASEM_NAMESPACE;
+
+using steady_timer = typename net::steady_timer::rebind_executor<net::io_context::executor_type>::other;
+
+// imitate a barrier using a timer
+struct tcondvar
+{
+  steady_timer tim;
+  tcondvar(net::io_context::executor_type exec) : tim(exec) {}
+
+  net::io_context::executor_type get_executor() {return tim.get_executor();}
+
+  template<typename Predicate, typename Handler>
+  auto async_wait(Predicate && p, Handler && h)
+  {
+    return net::async_compose<Handler, void(error_code)>(
+        [this, p = std::move(p), initial = true](auto && self, error_code = {}) mutable
+        {
+          if (initial)
+          {
+            initial = false;
+            return net::post(std::move(self));
+          }
+          error_code ec;
+          if (self.cancelled() != net::cancellation_type::none)
+            ec = net::error::operation_aborted;
+          if (ec || p())
+            self.complete(ec);
+          else
+            tim.async_wait(std::move(self));
+
+        }, h, tim.get_executor());
+  }
+
+  void notify_one()
+  {
+    tim.cancel_one();
+  }
+
+
+  void notify_all()
+  {
+    tim.cancel();
+  }
+};
+
+
+template<typename CondVar>
+void run_benchmark(asio::io_context::executor_type exec, std::size_t n)
+{
+  CondVar cv{exec};
+
+  struct impl
+  {
+    CondVar & cv;
+    void operator()(error_code ec, std::size_t i)
+    {
+      if (i != 0u)
+        cv.async_wait([&i = i]{return i % 4 == 0;}, net::detached);
+
+
+      if (i % 100 == 0)
+        cv.notify_all();
+      else
+        cv.notify_one();
+      if (i != 0)
+        net::post(cv.get_executor(), net::append(*this, ec, i - 1));
+    };
+  };
+
+  net::post(exec, net::append(impl{cv}, error_code(), n));
+
+  exec.context().run();
+}
+
+struct benchmark
+{
+  const char * name;
+  std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+  benchmark(const char * name) : name(name)
+  {
+  }
+
+  ~benchmark()
+  {
+    auto end = std::chrono::steady_clock::now();
+
+    printf("Benchmark  %s: %ld us\n", name, std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
+  }
+
+  operator bool () const {return true; }
+};
+
+int main(int argc, char * argv[])
+{
+  {net::io_context ctx{1}; }
+  const std::size_t cnt = 1'000'000;
+  if (auto b = benchmark("no-mutex asio"))
+  {
+    asio::io_context ctx{1};
+    run_benchmark<tcondvar>(ctx.get_executor(), cnt);
+  }
+
+  if (auto b = benchmark("no-mutex asem"))
+  {
+    asio::io_context ctx{1};
+    run_benchmark<basic_condition_variable<net::io_context::executor_type>>(ctx.get_executor(), cnt);
+  }
+  if (auto b = benchmark("mutexed  asio"))
+  {
+    asio::io_context ctx{-1};
+    run_benchmark<tcondvar>(ctx.get_executor(), cnt);
+  }
+
+  if (auto b = benchmark("mutexed  asem"))
+  {
+    asio::io_context ctx{-1};
+    run_benchmark<basic_condition_variable<net::io_context::executor_type>>(ctx.get_executor(), cnt);
+  }
+
+  return 0;
+}
---
+++ bench/mutex.cpp
@@ -0,0 +1,136 @@
+// Copyright (c) 2023 Klemens D. Morgenstern
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/asem/mutex.hpp>
+
+#if defined(BOOST_ASEM_STANDALONE)
+#include <asio/steady_timer.hpp>
+#include <asio/experimental/channel.hpp>
+#include <asio/experimental/concurrent_channel.hpp>
+#include <asio/compose.hpp>
+#include <asio/coroutine.hpp>
+#include <asio/detached.hpp>
+#include <asio/yield.hpp>
+
+#else
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/experimental/channel.hpp>
+#include <boost/experimental/concurrent_channel.hpp>
+#include <boost/asio/compose.hpp>
+#include <boost/asio/coroutine.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/yield.hpp>
+
+#endif
+
+using namespace BOOST_ASEM_NAMESPACE;
+
+// imitate a barrier using a timer
+template<template <typename ...> class Channel>
+struct tmutex
+{
+  Channel<void(error_code)> chan;
+  tmutex(net::io_context::executor_type exec) : chan(exec, 1) {}
+
+  template<typename Handler>
+  auto async_lock(Handler && h)
+  {
+    return chan.async_send(error_code(), std::move(h));
+  }
+
+  bool try_lock()
+  {
+    return chan.try_send(error_code());
+  }
+
+  void unlock()
+  {
+    chan.try_receive([](error_code) {});
+  }
+
+
+};
+
+template<typename Mutex>
+struct run_benchmark_impl : net::coroutine
+{
+  std::size_t N;
+  Mutex &mtx;
+
+  void operator()(error_code ec = {})
+  {
+    reenter (this)
+    {
+      while ( 0 < N--)
+      {
+        if (!mtx.try_lock())
+          yield mtx.async_lock(std::move(*this));
+        mtx.unlock();
+      }
+    }
+
+  }
+};
+
+template<typename Mutex>
+void run_benchmark(asio::io_context::executor_type exec, std::size_t n)
+{
+  Mutex cv{exec};
+
+  net::post(exec, run_benchmark_impl<Mutex>{{}, n, cv});
+
+  exec.context().run();
+
+}
+
+struct benchmark
+{
+  const char * name;
+  std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+  benchmark(const char * name) : name(name)
+  {
+  }
+
+  ~benchmark()
+  {
+    auto end = std::chrono::steady_clock::now();
+    printf("Benchmark  %s: %ld us\n", name,
+           std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
+  }
+
+  operator bool () const {return true; }
+};
+
+
+int main(int argc, char * argv[])
+{
+  {net::io_context ctx{1};}
+  const std::size_t cnt = 1'000'000;
+  if (auto b = benchmark("no-mutex asio"))
+  {
+    net::io_context ctx{1};
+    run_benchmark<tmutex<net::experimental::channel>>(ctx.get_executor(), cnt);
+  }
+
+  if (auto b = benchmark("no-mutex asem"))
+  {
+    net::io_context ctx{1};
+    run_benchmark<basic_mutex<net::io_context::executor_type>>(ctx.get_executor(), cnt);
+  }
+
+  if (auto b = benchmark("mutexed asio"))
+  {
+    net::io_context ctx{-1};
+    run_benchmark<tmutex<net::experimental::concurrent_channel>>(ctx.get_executor(), cnt);
+  }
+
+  if (auto b = benchmark("mutexed asem"))
+  {
+    net::io_context ctx{-1};
+    run_benchmark<basic_mutex<net::io_context::executor_type>>(ctx.get_executor(), 1'000'000);
+  }
+  return 0;
+}

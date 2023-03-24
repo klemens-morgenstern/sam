515b5435e07c4f1207185def5e5225b34a989c6e
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2023-03-24 20:14:45 +0800

    win concurrency_hint fix.

--- include/boost/sam/detail/concurrency_hint.hpp
+++ include/boost/sam/detail/concurrency_hint.hpp
@@ -20,21 +20,21 @@ namespace detail
 {
 inline bool is_single_threaded(net::execution_context &ctx)
 {
-
 #if defined(BOOST_SAM_STANDALONE)
   constexpr static int asio_concurrency_1 = ASIO_CONCURRENCY_HINT_1;
 #else
   constexpr static int asio_concurrency_1 = BOOST_ASIO_CONCURRENCY_HINT_1;
 #endif
-
   if (net::has_service<net::detail::scheduler>(ctx))
     return net::use_service<net::detail::scheduler>(ctx).concurrency_hint() == asio_concurrency_1;
+  else if (net::has_service<net::detail::io_context_impl>(ctx))
+    return net::use_service<net::detail::io_context_impl>(ctx).concurrency_hint() == asio_concurrency_1;
   else
     return false;
 }
 
 // only io_context or strands can be thread-safe
-inline bool is_single_threaded(net::any_io_executor exec)
+inline bool is_single_threaded(net::any_io_executor exec, boost::source_location loc = BOOST_CURRENT_LOCATION)
 {
   using namespace net;
   return is_single_threaded(exec.context());
